7575e21c81048c5c1ea6cbb035e592cdfe85870c
Lina Jo <lina.jo90@gmail.com>
2025-06-27 11:39:46 +0700

    modify runner so it generate reports

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -8,9 +8,9 @@ import org.testng.annotations.Test;
         features = "src/test/resources/features",           // Lokasi file .feature
         glue = "stepdef",                     // Package berisi Step Definitions
         plugin = {
-                "pretty" //,
-//                "html:reports/test-report.html",        // Output HTML
-//                "json:reports/test-report.json",                // Output JSON
+                "pretty",
+                "html:reports/test-report.html",        // Output HTML
+                "json:reports/test-report.json" //,                // Output JSON
 //                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm" // Untuk Allure report
         } //,
 //        monochrome = true,
